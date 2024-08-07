b4b3824fc24cdfe11823b26584adfa1e22ac0692
hamza <hamza@hamza-HP-Notebook>
2024-08-07 18:40:44 +0100

    dispaly info

---
+++ affichage_info.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char Pré_Nm[] = "hamza";
+    char Nm[] = "boumanjel";
+    int Age = 22 ;
+    char Sex = 'H';
+    char Nem_Tele [] = "0637157891";
+    printf("je m'appelle : %s , %s \n",Pré_Nm,Nm);
+    printf("---------------------------\n");
+    printf("mon sexe est : %c\n",Sex);
+    printf("----------------------------\n");
+    printf("j'ai : %d\n",Age);
+    printf("----------------------------\n");
+    printf("mon numéro de téléphone est : %s\n",Nem_Tele);
+    return 0;
+}
+
